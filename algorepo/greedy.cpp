f46b2bf29241898dd3772eef811d618c3dc5e038
GXM_lab <gxm1015@outlook.com>
2022-09-29 11:34:52 +0800

    435_OK

--- algorepo/greedy.cpp
+++ algorepo/greedy.cpp
@@ -50,3 +50,25 @@ void test_135()
     vector<int> ratings = {1,3,4,5,2};
     candy(ratings);
 }
+
+// 435.Non-overlapping Intervals
+int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+  if(intervals.size() < 2)
+    return 0;
+  
+  sort(intervals.begin(), intervals.end(), \
+  [](vector<int> a, vector<int> b){
+    return a[1] < b[1];
+  });
+
+  int erase_total = 0;
+  int pivot = intervals[0][1];
+  for(int i = 1; i < intervals.size(); i++){
+    if(intervals[i][0] >= pivot)
+      pivot = intervals[i][1];
+    else
+      erase_total++;
+  }
+
+  return erase_total;
+}

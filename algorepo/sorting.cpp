13e3367888f1e5a21009ed2672d7ec002e405269
GXM_lab_ubuntu22 <gxm1015@outlook.com>
2022-10-20 10:04:41 +0800

    451_OK

--- algorepo/sorting.cpp
+++ algorepo/sorting.cpp
@@ -173,10 +173,10 @@ vector<int> topKFrequent(vector<int>& nums, int k)
   }
 
   vector<int> res;
-  for( int i = max_count; i >= 0 && res.size() < k; i-- ){
+  for( int i = max_count; i >= 0 && (int)res.size() < k; i-- ){
     for( const auto & num : fre_order[i] ){
       res.push_back(num);
-      if( res.size() == k )
+      if( (int)res.size() == k )
         break;
     }
   }
@@ -185,9 +185,46 @@ vector<int> topKFrequent(vector<int>& nums, int k)
 
 
 // 451. Sort Characters By Frequency
+string frequencySort_V1(string s)
+{
+  unordered_map<char, int> freq;
+  vector<string> bucket(s.size()+1, "");
+  string res;
+
+  // Count frequency of each character
+  for(char c: s)
+    freq[c]++;
+  
+  // Put character in frequency bucket
+  for(auto& item : freq){
+    int n = item.second;
+    int c = item.first;
+    bucket[n].append(n, c);
+  }
+
+  // Form the descending sorted string
+  for( int i = s.size(); i > 0; i--){
+    if(!bucket[i].empty())
+      res.append(bucket[i]);
+  }
+  
+  return res;
+}
+string frequencySort_V2(string s)
+{
+  int count[256] = {0};
+  for( char c: s)
+    count[c]++;
+
+  // in the lamda func, "[&]" allow you to use vars in main body
+  sort(s.begin(), s.end(), [&](char a, char b){
+    return count[a] > count[b] || (count[a]==count[b] && a < b);
+  });
+
+  return s;
+}
 
 
-
-
+// 75. Sort Colors
 
 

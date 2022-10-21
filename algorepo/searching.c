662835d87bebc001665dbadcc574cdb407f8161c
GXM_lab_ubuntu22 <gxm1015@outlook.com>
2022-10-21 16:12:28 +0800

    75_OK

---
+++ algorepo/searching.c
@@ -0,0 +1,4 @@
+#include "include/algorepo.h"
+#include "include/testcase.h"
+
+// 695. Max Area of Island
--- algorepo/sorting.cpp
+++ algorepo/sorting.cpp
@@ -226,5 +226,22 @@ string frequencySort_V2(string s)
 
 
 // 75. Sort Colors
+void sortColors_V1(vector<int>& nums)  // 桶排序
+{
+  int count[3]={0, 0, 0};
+  for(int i = 0; i < nums.size(); i++)
+    count[nums[i]]++;
 
-
+  for(int i = 0; i < nums.size(); i++){
+    if(count[0]-- > 0)
+      nums[i] = 0;
+    else if(count[1]-- > 0)
+      nums[i] = 1;
+    else
+      nums[i] = 2;
+  }
+}
+void sortColor_V2(vector<int>& nums)  // 多指针
+{
+  
+}
